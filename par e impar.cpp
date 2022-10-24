0cf0ffcbc59c711ea724b6a6d029611632b9d798
Mchilel <115515044+Mchilel@users.noreply.github.com>
2022-10-23 18:20:03 -0900

    Add files via upload

---
+++ par e impar.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout <<"ingrese un numero:";
+	cin >> n;
+	
+	if (n % 2 == 0)
+	cout << "El numero es par";
+	else
+	cout <<"El numero es impar";
+	
+}
